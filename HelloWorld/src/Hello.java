f341185fd48c86f011248920d918f10f4381995b
Arnas Kundrotas <arnas.kundrotas@gmail.com>
2020-12-26 17:13:08 +0200

    update hello world project

--- HelloWorld/src/Hello.java
+++ HelloWorld/src/Hello.java
@@ -3,5 +3,23 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("I'm Arnas");
+
+        int myFirstNumber = 5 + 5;
+        System.out.println("My First Number " + myFirstNumber);
+
+        int mySecondNumber = 12;
+        int myThirdNumber = 6;
+        int sum = mySecondNumber + myThirdNumber;
+        System.out.println("My sum of two numbers " + sum);
+
+        // Java converts numbers to strings
+        String myFirstString = "abc";
+        String converted = mySecondNumber + myThirdNumber + myFirstString;
+        System.out.println("My sum of two numbers and string converted " + converted);
+
+        int myLastOne = sum - 1000;
+        System.out.println(myLastOne);
+
     }
 }
+
