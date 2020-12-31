61eb1ac298d545344bd0c7e6ebb3635d0b6989f7
Arnas Kundrotas <arnas.kundrotas@gmail.com>
2020-12-31 10:59:33 +0200

    update project

--- Person/src/com/arnaskundrotas/Main.java
+++ Person/src/com/arnaskundrotas/Main.java
@@ -3,6 +3,33 @@ package com.arnaskundrotas;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+	    Person person = new Person();
+
+	    person.setFirstName("");
+	    person.setLastName("");
+	    person.setAge(10);
+
+        System.out.println("fullName = " + person.getFullName());
+        System.out.println("teen = " + person.isTeen());
+
+        person.setFirstName("Arnas");
+        person.setLastName("Kundrotas");
+        person.setAge(50);
+
+        System.out.println("fullName = " + person.getFullName());
+        System.out.println("age = " + person.getAge());
+        System.out.println("teen = " + person.isTeen());
+
+        person.setFirstName("");
+        person.setLastName("Kundrotas");
+
+        System.out.println("fullName = " + person.getFullName());
+
+        person.setFirstName("Arnas");
+        person.setLastName("Kundrotas");
+
+        System.out.println("fullName = " + person.getFullName());
+
     }
 }
--- Person/src/com/arnaskundrotas/Person.java
+++ Person/src/com/arnaskundrotas/Person.java
@@ -1,13 +1,69 @@
 package com.arnaskundrotas;
 
+import java.awt.datatransfer.StringSelection;
+
 public class Person {
 
     private String firstName;
     private String lastName;
     private int age;
 
+    // SET
     public void setFirstName (String firstName) {
-        
+        this.firstName = firstName;
     }
+
+    public void setLastName (String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setAge (int age) {
+        this.age = age;
+    }
+
+    // GET
+
+    public String getFirstName (){
+        return firstName;
+    }
+
+    public String getLastName (){
+        return lastName;
+    }
+
+    public int getAge (){
+        if (age < 0 || age > 100) {
+            return 0;
+        }
+        return age;
+    }
+
+    public boolean isTeen (){
+        if (age > 12 && age < 20) {
+            return true;
+        }
+        return false;
+    }
+
+    public String getFullName (){
+        if (firstName.isEmpty() && lastName.isEmpty()) {
+            return " ";
+        }
+        if (lastName.isEmpty()) {
+            return firstName;
+        }
+
+        if (firstName.isEmpty()) {
+            return lastName;
+        }
+
+        return firstName + " " + lastName;
+    }
+
+
+
+
+
+
 
 }
